5ce7e6d8847d68598deb068196f312d17b49c406
LouisDC98 <louisdacosta98@gmail.com>
2021-06-14 17:34:04 +0200

    Add min length validation and search in ingredients for main input

--- js/search/SearchServices/MainSearch.js
+++ js/search/SearchServices/MainSearch.js
@@ -2,12 +2,22 @@ import recipes from '../../data/recipes.js';
 
 class MainSearch {
   static research(searchParams) {
-    const filteredRecipes = [];
-    recipes.forEach((element) => {
-      if (element.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase().indexOf(searchParams.main) > -1) {
-        filteredRecipes.push(element);
-      } else if (element.description.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase().indexOf(searchParams.main) > -1) {
-        filteredRecipes.push(element);
+    let filteredRecipes = [];
+    if (searchParams.main.length <= 2) {
+      return filteredRecipes = [...recipes];
+    }
+    recipes.forEach((recipe) => {
+      if (recipe.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase().indexOf(searchParams.main) > -1) {
+        filteredRecipes.push(recipe);
+      }
+      else if (recipe.description.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase().indexOf(searchParams.main) > -1) {
+        filteredRecipes.push(recipe);
+      }
+      else if (
+        recipe.ingredients.forEach(element => {
+          element.ingredient.indexOf(searchParams.main) > -1})
+          ) {
+        filteredRecipes.push(recipe);
       }
     });
     return filteredRecipes;
